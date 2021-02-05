fb4015b7c7e757ef025f2b37fe11e7571b4c917f
akalisvm <2722968218@qq.com>
2021-02-05 10:00:09 +0800

    update

---
+++ js/iframe-self-adaption.js
@@ -0,0 +1,17 @@
+function calcPageHeight(doc) {
+    var cHeight = Math.max(doc.body.clientHeight, doc.documentElement.clientHeight)
+    var sHeight = Math.max(doc.body.scrollHeight, doc.documentElement.scrollHeight)
+    var height  = Math.max(cHeight, sHeight)
+    return height
+}
+
+var ifr = document.getElementById('ifr')
+ifr.onload = function() {
+      ifr.style.height='0px';
+    var iDoc = ifr.contentDocument || ifr.document
+    var height = calcPageHeight(iDoc)
+    if(height < 850){
+        height = 850;
+    }
+    ifr.style.height = height + 'px'
+}
